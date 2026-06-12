4abfaa6711895057a98b41c2088105b8fce22c72
kamallearner123 <kamalbec2004@gmail.com>
2026-06-12 05:51:17 +0530

    Update project files

---
+++ Project/space_defender/build/browser/CMakeFiles/game.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/game.dir/ai/ai.cpp.o"
+  "CMakeFiles/game.dir/ai/ai.cpp.o.d"
+  "CMakeFiles/game.dir/browser/main_browser.cpp.o"
+  "CMakeFiles/game.dir/browser/main_browser.cpp.o.d"
+  "CMakeFiles/game.dir/engine/engine.cpp.o"
+  "CMakeFiles/game.dir/engine/engine.cpp.o.d"
+  "game.js"
+  "game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
